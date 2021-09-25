db3acf4693b2a2e31485ed8d3ed1508edd5e92c9
Abhinav Marwaha <abhinavmarwaha.bt19cse@pec.edu.in>
2021-09-25 17:16:11 +0530

    hosting.bat

---
+++ hosting.bat
@@ -0,0 +1,17 @@
+git checkout main
+git pull 
+git checkout hosting
+git checkout main -- config
+git checkout main -- models
+git checkout main -- scrapers
+git checkout main -- server
+git checkout main -- utils
+git checkout main -- .gitignore
+git checkout main -- app.py
+git checkout main -- .Procfile
+git checkout main -- requirements.txt
+git checkout main -- runtime.txt
+git checkout main -- server_requirements.txt
+git checkout main -- server_setup.sh
+git commit -m "v0.1"
+git push
